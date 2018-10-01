9975e17c52238c743c244a1498a6e285b42d17a6
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2018-10-01 11:52:18 -0300

    Compare normals

--- trueskill/mathematics.py
+++ trueskill/mathematics.py
@@ -14,18 +14,53 @@ from __future__ import absolute_import
 
 import copy
 import math
+import numpy as np
 try:
     from numbers import Number
 except ImportError:
     Number = (int, long, float, complex)
 
 from six import iterkeys
+from scipy.stats import norm
 
 
 __all__ = ['Gaussian', 'Matrix', 'inf']
 
 
 inf = float('inf')
+
+
+def compare_normal(normal1, normal2):
+    """
+    Returns the probability of the true value of a normally distributed random variable being bigger
+    than the true value of another normally distributed random variable
+
+    :param tuple normal1: (mean, std) of the first normal
+    :param tuple normal2: (mean, std) of the second normal
+    :return: P(Normal1 > Normal2)
+    :rtype: float
+    """
+    n_samples = 1000
+
+    mu1, sigma1 = normal1
+    mu2, sigma2 = normal2
+
+    start = mu2 - 4 * sigma2
+    stop = mu2 + 4 * sigma2
+    step_size = sigma2 / 10
+    err = step_size / 2
+
+    P_vector = []
+    x = start
+    while x <= stop:
+        samples = np.random.normal(loc=mu1, scale=sigma1, size=n_samples)
+        bigger = len([sample for sample in samples if sample > x])
+        P_x = norm.cdf(x + err, loc=mu2, scale=sigma2) - norm.cdf(x - err, loc=mu2, scale=sigma2)
+
+        P_vector.append(P_x * (bigger / n_samples))
+        x += step_size
+
+    return sum(P_vector)
 
 
 class Gaussian(object):
@@ -68,19 +103,19 @@ class Gaussian(object):
     __div__ = __truediv__  # for Python 2
 
     def __eq__(self, other):
-        return self.pi == other.pi and self.tau == other.tau
+        return compare_normal((self.mu, self.sigma), (other.mu, other.sigma)) == 0.5
 
     def __lt__(self, other):
-        return self.mu < other.mu
+        return compare_normal((self.mu, self.sigma), (other.mu, other.sigma)) < 0.5
 
     def __le__(self, other):
-        return self.mu <= other.mu
+        return compare_normal((self.mu, self.sigma), (other.mu, other.sigma)) <= 0.5
 
     def __gt__(self, other):
-        return self.mu > other.mu
+        return compare_normal((self.mu, self.sigma), (other.mu, other.sigma)) > 0.5
 
     def __ge__(self, other):
-        return self.mu >= other.mu
+        return compare_normal((self.mu, self.sigma), (other.mu, other.sigma)) >= 0.5
 
     def __repr__(self):
         return 'N(mu=%.3f, sigma=%.3f)' % (self.mu, self.sigma)
